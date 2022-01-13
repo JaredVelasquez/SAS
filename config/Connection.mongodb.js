6e3f56f96d1c2bcf26e6eea772948f5e28d6a51e
JaredVelasquez <jared.velasquez1122@gmail.com>
2022-01-12 21:37:56 -0600

    Add heroku

--- config/Connection.mongodb.js
+++ config/Connection.mongodb.js
@@ -2,7 +2,7 @@ let mongoose = require("mongoose");
 let dbConnection = require("../env/devConnection");
 
 mongoose.connect(
-    "mongodb+srv://Jared:3S38hPyDnWQbXNwj@cluster0.4swhp.mongodb.net/SAS?retryWrites=true&w=majority",
+    process.env.MONGODB,
     {
         useNewUrlParser: true, 
         useUnifiedTopology: true,
