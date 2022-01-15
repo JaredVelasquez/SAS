86906482264e26b48e965634525ce7a5e84660a4
JaredVelasquez <jared.velasquez1122@gmail.com>
2022-01-15 11:11:39 -0600

    fix problen server

--- config/Connection.mongodb.js
+++ config/Connection.mongodb.js
@@ -1,5 +1,4 @@
 let mongoose = require("mongoose");
-let dbConnection = require("../env/devConnection");
 
 mongoose.connect(
     "mongodb+srv://Jared:3S38hPyDnWQbXNwj@cluster0.4swhp.mongodb.net/SAS?retryWrites=true&w=majority",
